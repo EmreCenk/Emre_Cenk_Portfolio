00078551ed03f37edd29c9b499fad236424e3b5e
Emre Cenk <emrecenk9@gmail.com>
2021-05-28 20:42:27 -0400

    resolved compatability issues with the carousel imports and current css

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function insert_portfolio(list_of_images, id_to_insert){
+    
+
+
+    let template = document.getElementById("carouselExampleControls");
+    let content = template.content;
+    
+}
