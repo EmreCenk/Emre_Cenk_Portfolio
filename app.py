5c50db83a0e9e37e9d719b7eff99b778e2f1cc2b
Emre Cenk <emrecenk9@gmail.com>
2021-06-07 00:50:42 -0400

    b

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
-app = Flask(__name__)
+app = Flask(__name__, template_folder="", static_folder="")
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now()
-
-    return f"""
-    <h1>Hello heroku</h1>
-    <p>It is currently {the_time}.</p>
-
     
-    """
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
