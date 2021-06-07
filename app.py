0a3c2758cf9bc65c7b3a52e69bd8b519bc2cc6e7
Emre Cenk <emrecenk9@gmail.com>
2021-06-07 00:54:15 -0400

    for some reason, changing the name to 'app' fixed my problem

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder="", static_folder="")
 @app.route('/')
 def homepage():
     
-    return render_template("index.html")
+    return render_template()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
