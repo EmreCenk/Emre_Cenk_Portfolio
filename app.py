de723d220789f50905772fb7de9ff094dc375240
Emre Cenk <emrecenk9@gmail.com>
2021-06-07 00:42:18 -0400

    a

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
 
-from flask import Flask,render_template, request
-import os
-from flask.templating import render_template
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    the_time = datetime.now()
 
-website_app = Flask(__name__, template_folder="", static_folder="") #overriding the default directories flask searches for
+    return f"""
+    <h1>Hello heroku</h1>
+    <p>It is currently {the_time}.</p>
 
-print("app qweqweqwe!")
-
-@website_app.route('/',methods=["POST","GET"])
-def index():
-    return render_template("index.html")
     
-# @website_app.errorhandler(404)
-# def handler(a):
-#     return render_template("index.html")
+    """
 
-print("a")
-website_app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
-print("beta")
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
+
+# from flask import Flask,render_template, request
+# import os
+# from flask.templating import render_template
+
+
+# website_app = Flask(__name__, template_folder="", static_folder="") #overriding the default directories flask searches for
+
+# print("app qweqweqwe!")
+
+# @website_app.route('/',methods=["POST","GET"])
+# def index():
+#     return render_template("index.html")
+    
+# # @website_app.errorhandler(404)
+# # def handler(a):
+# #     return render_template("index.html")
+
+# print("a")
+# website_app.run(host=os.getenv('IP', '0.0.0.0'), 
+#             port=int(os.getenv('PORT', 4444)))
+# print("beta")
