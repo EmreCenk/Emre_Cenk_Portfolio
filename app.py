c3a0aabe4dfb20941af2590ac650b926a08c4cf7
Emre Cenk <emrecenk9@gmail.com>
2021-06-07 00:24:59 -0400

    a

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask.templating import render_template
 
 website_app = Flask(__name__, template_folder="", static_folder="") #overriding the default directories flask searches for
 
-print("app running!")
+print("app qweqweqwe!")
 
 @website_app.route('/',methods=["POST","GET"])
 def index():
@@ -15,7 +15,8 @@ def index():
 # @website_app.errorhandler(404)
 # def handler(a):
 #     return render_template("index.html")
-    
+
+print("a")
 if __name__ == "__main__":
     print("here.")
     website_app.run(debug = False);
